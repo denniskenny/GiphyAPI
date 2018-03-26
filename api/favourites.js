e8cec4845191f4f19580d6a0db07243479890699
Dennis Kenny <denniskenny@users.noreply.github.com>
2018-03-26 17:54:02 +0100

    Initial Version

---
+++ api/favourites.js
@@ -0,0 +1,3 @@
+let favourites = []
+
+export { favourites };
---
+++ src/app/giphyService.js
@@ -0,0 +1,43 @@
+/*eslint-disable no-unused-vars */
+/*eslint-disable no-console */
+const giphyService = function($http) {
+
+    let returnVal = {};
+
+    returnVal.searchApi = function(searchTerm, offset) {
+
+        if (!offset) offset = 0;
+        return $http.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=dc6zaTOxFJmzC&limit=15&offset=${offset}`);
+
+    };
+
+    returnVal.addToFavourites = function(data) {
+
+        $http.post('/api/favourites', data).then(
+            function(success) {},
+            function(fail) {
+                console.log(fail);
+            });
+    };
+
+    returnVal.removeFromFavourites = function(data) {
+
+        $http.delete(`/api/favourites?id=${data.id}`).then(
+            function(success) {},
+            function(fail) {
+                console.log(fail);
+            });
+    };
+
+
+
+
+    returnVal.getFavourites = function() {
+
+        return $http.get('/api/favourites');
+    };
+
+    return returnVal;
+};
+
+export default giphyService;
